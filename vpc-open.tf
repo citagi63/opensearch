2b0012b83ed197f20abe1e5015ace5749ee20ee9
citagi63 <root@ip-172-31-9-184.us-west-2.compute.internal>
2022-11-16 10:11:53 +0000

    new future

--- vpc-open.tf
+++ vpc-open.tf
@@ -10,13 +10,13 @@ resource "aws_vpc" "domain" {
   cidr_block = "10.0.0.0/16"
 }
 resource "aws_subnet" "domain_subnet" {
-  vpc_id           = aws_vpc.domain.id
-  cidr_block       = "10.0.1.0/24"
+  vpc_id            = aws_vpc.domain.id
+  cidr_block        = "10.0.1.0/24"
   availability_zone = "us-west-1a"
 }
 resource "aws_subnet" "domain_pu_subnet" {
-  vpc_id           = aws_vpc.domain.id
-  cidr_block       = "10.0.2.0/24"
+  vpc_id            = aws_vpc.domain.id
+  cidr_block        = "10.0.2.0/24"
   availability_zone = "us-west-1b"
 }
 resource "aws_internet_gateway" "domain_iwg" {
@@ -60,17 +60,17 @@ resource "aws_route_table_association" "private_route" {
   route_table_id = aws_route_table.domin_route-private.id
 }
 resource "aws_security_group" "es" {
-  name        = "${var.vpc}-elasticsearch-${var.domain}"
+  name        = "elasticsearch-var.domain"
   description = "Managed by Terraform"
-  vpc_id = aws_vpc.domain.id
-   ingress {
+  vpc_id      = aws_vpc.domain.id
+  ingress {
     from_port = 443
     to_port   = 443
     protocol  = "tcp"
     cidr_blocks = [
       aws_vpc.domain.cidr_block,
     ]
-}
+  }
 }
 resource "aws_iam_service_linked_role" "es" {
   aws_service_name = "es.amazonaws.com"
@@ -81,14 +81,46 @@ resource "aws_elasticsearch_domain" "opensearch" {
   elasticsearch_version = "7.10"
 
   cluster_config {
-    instance_type  = "m3.medium.search"
-    instance_count = "1"
+    instance_type          = "m3.medium.search"
+    instance_count         = "1"
     zone_awareness_enabled = "true"
   }
-   vpc_options { 
-	subnet_ids = [
-	   aws_subnet.domain_subnet.id
-]
-	security_group_ids = [aws_security_group.es.id]
+  ebs_options {
+    ebs_enabled = "true"
+    volume_size = "45"
+    volume_type = "gp2"
+  }
+   node_to_node_encryption {
+    enabled = true
+  }
+  vpc_options {
+    subnet_ids = [
+      aws_subnet.domain_subnet.id
+    ]
+    security_group_ids = [aws_security_group.es.id]
+
+  }
+ domain_endpoint_options {
+    enforce_https = "true"
+    tls_security_policy = "Policy-Min-TLS-1-0-2019-07"
+  }
+  advanced_options = {
+    "rest.action.multi.allow_explicit_index" = "true"
+  }
+  access_policies = <<CONFIG
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": "es:*",
+            "Principal": "*",
+            "Effect": "Allow",
+            "Resource": "arn:aws:es:domain/*"
+        }
+    ]
+}
+CONFIG
+  depends_on      = [aws_iam_service_linked_role.es]
 
 }
+
